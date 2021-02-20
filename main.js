cea97e273226f85bc8f4adf1ff89afea912b2466
Omnivoro <ominivor@gmail.com>
2021-02-20 16:56:45 +0000

    fix: Limit to 5 the amount of requested posts
    https://jsonplaceholder.typicode.com/todos?_limit=5

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 // GET REQUEST
-function getTodos() {axios({method: 'get',
-		   url: 'https://jsonplaceholder.typicode.com/todos'})
+function getTodos()
+	{axios({method: 'get',
+		    url: 'https://jsonplaceholder.typicode.com/todos',
+		    params: { _limit: 5}})
 		.then(res => showOutput(res))
 		.catch(err => console.log(err));
 }
