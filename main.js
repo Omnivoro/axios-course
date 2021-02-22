1fe785c59733a5f4776a831febd1eda69369db86
Omnivoro <ominivor@gmail.com>
2021-02-22 13:07:34 +0000

    fix: Use the delete() method after the axios function

--- main.js
+++ main.js
@@ -23,8 +23,7 @@ function updateTodo() {
 
 // DELETE REQUEST
 function removeTodo() {
-  axios({method: 'delete',
-  		url: 'https://jsonplaceholder.typicode.com/todos/1'})
+  axios.delete('https://jsonplaceholder.typicode.com/todos/1')
   		.then(res => showOutput(res))
 		.catch(err => console.log(err));
 }
